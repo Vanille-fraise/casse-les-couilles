a15d88ad8bd1f9bbdb6fda9b474e219adafffbf7
edffarrando <edffarrando@gmail.com>
2025-04-12 22:14:09 +0000

    fix for vercel

--- src/components/FaceBackgroundExample.tsx
+++ src/components/FaceBackgroundExample.tsx
@@ -34,10 +34,11 @@ const placeholderImageUrls = [
 
 // It's better to have a variety of images for a real application
 // Example using unsplash placeholders (replace with real images)
+/*
 const exampleImageUrls = Array.from({ length: 25 }, (_, i) => {
     const randomId = Math.floor(Math.random() * 1001); // Generate random number between 0 and 1000
     return `https://picsum.photos/id/${randomId}/300/200`});
-
+*/
 
 const FaceBackgroundExample: React.FC = () => {
   // You can switch to exampleImageUrls or provide your actual image URLs
